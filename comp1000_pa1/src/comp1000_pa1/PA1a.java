b17e4d726c0a025056f37b1b7c11141a5553333a
lanec4atwit <113481547+lanec4atwit@users.noreply.github.com>
2022-09-21 19:41:56 -0400

    Update PA1a.java

--- comp1000_pa1/src/comp1000_pa1/PA1a.java
+++ comp1000_pa1/src/comp1000_pa1/PA1a.java
@@ -1,5 +1,30 @@
-package comp1000_pa1;
+import java.util.Scanner;
 
-public class PA1a {
+public class PA1pt2 {
+
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		
+
+		
+		int yards, feet, inches;
+
+	System.out.println("Enter the Amount of inches Below");
+		inches = userInput.nextInt();
+		
+		//This is the end of the Collection. The rest of the code will be the conversion
+		
+			yards = inches / 36;
+			inches = inches % 36;
+			
+			feet = inches / 12;
+			inches = inches % 12;
+			
+		System.out.println("The number of Yards is " + yards);
+		
+		System.out.println("The number of feet is: " + feet);
+		
+		System.out.println("The number of Inches is: " + inches);
+	}
 
 }
